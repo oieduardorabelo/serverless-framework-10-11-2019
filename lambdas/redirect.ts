5c07b55faec463da573db6f37affcdc580574712
Eduardo Rabelo <oieduardorabelo@gmail.com>
2019-11-10 19:38:20 +1300

    save

--- lambdas/redirect.ts
+++ lambdas/redirect.ts
@@ -1,17 +1,39 @@
 import "source-map-support/register";
 import { APIGatewayProxyHandler } from "aws-lambda";
+import * as AWS from "aws-sdk";
+
+let ddbConfig = {
+  tableName: process.env.DDB_TABLE_NAME
+};
+let ddbClient = new AWS.DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
   console.log(JSON.stringify(event));
 
-  let target = "https://google.com";
+  let { slug } = event.pathParameters;
+
+  let linkRecord = await ddbClient
+    .get({
+      TableName: ddbConfig.tableName,
+      Key: {
+        slug
+      }
+    })
+    .promise();
+
+  if (linkRecord.Item) {
+    return {
+      statusCode: 302,
+      body: linkRecord.Item.long_link,
+      headers: {
+        Location: linkRecord.Item.long_link,
+        "Content-Type": "text/plain"
+      }
+    };
+  }
 
   return {
-    statusCode: 302,
-    body: target,
-    headers: {
-      Location: target,
-      "Content-Type": "text/plain"
-    }
+    statusCode: 404,
+    body: "Slug not found. Check that you entered it right."
   };
 };
